9f6937fd8ffe1e9101eafca757908500504e926e
c <lamoureux.camille@outlook.fr>
2019-11-22 13:30:13 +0100

    finish part 1 - 3

---
+++ variables.py
@@ -0,0 +1,10 @@
+# LAMOUREUX Camille - 291756
+# 2DTTL - Final Project
+
+import initialisation as i
+
+# Print attributes and types of "data"
+print("TYPES : ")
+print(i.data.dtypes)
+print("ATTRIBUTES : ")
+print(dir(i.data))
