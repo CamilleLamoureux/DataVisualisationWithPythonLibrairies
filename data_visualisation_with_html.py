029fa85fb9fb2f5f76099910d5a3baf350356eeb
c <lamoureux.camille@outlook.fr>
2019-11-22 13:13:12 +0100

    finish part 1 - 1 and 2

---
+++ data_visualisation_with_html.py
@@ -0,0 +1,8 @@
+import initialisation as i
+
+# Data visualization with html
+html_content = i.data.to_html()
+text_file = open("index.html", "w")
+text_file.write(html_content)
+text_file.close()
+i.webbrowser.open_new_tab("index.html")
---
+++ initialisation.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import webbrowser
+import matplotlib.pyplot as plt
+
+# Reading the csv file into a DF
+data = pd.read_csv('data.csv')
