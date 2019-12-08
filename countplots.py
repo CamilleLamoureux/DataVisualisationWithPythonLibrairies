8e769e543d08400ff864f4ee45aa191d97107341
c <lamoureux.camille@outlook.fr>
2019-12-08 14:05:54 +0100

    finish part 1 - 6

--- countplots.py
+++ countplots.py
@@ -4,11 +4,19 @@
 import initialisation as i
 
 # Selecting data to show
-countries = ['France','Italy','Spain','Brazil','Argentina','Germany','England']
-data_reduce = i.data.loc[i.data['Nationality'].isin(countries)]
+nationalities = ['France','Italy','Spain','Brazil','Argentina','Germany','England']
+data_reduce = i.data.loc[i.data['Nationality'].isin(nationalities)]
 
 # Creating bar plot
-i.sns.barplot(y='Potential', x='Nationality', data=data_reduce)
+i.sns.barplot(data=data_reduce, y='Nationality',x='Potential')
 
 # Display
 i.plt.show()
+
+'''
+NOTES :
+    Pays avec le plus haut potentiel est l'Espagne.
+    NB : On sait que dans le foot, les joueurs peuvent aller jouer dans d'autres pays que celui dont ils ont la nationalité, 
+    mais il est impossible avec les données présentes de trier par autre chose que la nationalité.
+    On utilise donc les valeurs "Potential" et "Nationality".
+'''
